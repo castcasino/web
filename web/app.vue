fdac4259f38e9ce7f5cd7f7a91ad5e3a20dcabaf
Ava's DAO <support@avasdao.org>
2022-12-31 07:25:43 -0500

    Upgrade to Nuxt v3.

---
+++ web/app.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="w-screen h-screen flex justify-center items-center bg-gradient-to-r from-purple-100 to-purple-200">
+        <h1 class="text-3xl text-purple-500 font-bold">
+            Nexa Games is coming soon...
+        </h1>
+    </main>
+</template>
