577137b7d3fd8416f5c76257d5d957c3c35dead2
Ava's DAO <support@avasdao.org>
2023-01-04 05:06:35 -0500

    Add Fairplay verification page.

--- web/pages/Blank.vue
+++ web/pages/Blank.vue
@@ -9,36 +9,6 @@ useHead({
 
 <template>
     <main class="min-h-full">
-        <SiteHeaderView />
-
-        <section class="-mt-24 pb-8">
-            <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h1 class="sr-only">
-                    Blank
-                </h1>
-
-                <!-- Main 3 column grid -->
-                <div class="grid grid-cols-1 gap-4 items-start lg:grid-cols-3 lg:gap-8">
-                    <section class="p-5 flex flex-col col-span-2 space-y-4 bg-yellow-50 border-2 border-yellow-200 rounded-lg shadow">
-                        <h1 class="text-4xl font-bold text-yellow-900">
-                            Blank
-                        </h1>
-
-                        <p class="text-yellow-900">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum aliquam maiores animi iusto est laborum quas dolore assumenda voluptate quis! Quaerat corrupti enim autem iusto voluptates dolorum quae voluptate eligendi.
-                        </p>
-                    </section>
-
-                    <!-- Right column -->
-                    <div class="grid grid-cols-1 gap-4">
-                        <PayoutsBlock />
-
-                        <BlockRewardsPanel />
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <FooterView />
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eius voluptatem minus natus at eveniet dolorum eos mollitia, maxime animi excepturi harum omnis illum odit recusandae pariatur! Unde, explicabo molestias.
     </main>
 </template>
---
+++ web/pages/Fairplay.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+useHead({
+    title: 'Blank — Nexa Games',
+    meta: [
+        { name: 'description', content: 'Nexa Gaming offers a revolutionary blockchain experience.' }
+    ],
+})
+</script>
+
+<template>
+    <Header />
+
+    <main class="max-w-5xl mx-auto my-10">
+        <h1 class="text-5xl font-medium">
+            Fairplay
+        </h1>
+
+        <p class="my-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur assumenda velit animi aut ipsa ab vero perferendis nam consequuntur sequi qui deleniti neque facere, laudantium dolore officiis earum tempore nesciunt.
+        </p>
+
+        <div class="w-full py-5 flex justify-center">
+            <input
+                type="text"
+                placeholder="Type or copy/paste your Player ID here"
+                class="w-1/2 py-2 px-5 bg-yellow-100 border-4 border-yellow-300 text-lg text-yellow-900 placeholder-yellow-900 rounded-lg"
+            />
+        </div>
+    </main>
+
+    <Footer />
+</template>
