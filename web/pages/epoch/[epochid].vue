3e3442c1e7c40f8c8ed79d628b6f281e0427800a
Shomari <nyusternie@sdot.io>
2023-05-13 15:10:39 -0400

    Create epoch page.

---
+++ web/pages/epoch/[epochid].vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+/* Initialize stores. */
+import { useSystemStore } from '@/stores/system'
+
+/* Initialize System. */
+const System = useSystemStore()
+
+useHead({
+    title: 'Epochs — Nexa Games',
+    meta: [
+        { name: 'description', content: 'Nexa Gaming offers a revolutionary blockchain experience.' }
+    ],
+})
+
+const route = useRoute()
+
+const epochid = route.params.epochid
+console.log('EPOCH ID', epochid)
+
+</script>
+
+<template>
+    <Header />
+
+    <main class="max-w-5xl mx-auto my-10">
+        <h1 class="text-3xl font-medium">
+            Epoch #{{epochid}} Attestation
+        </h1>
+
+        <pre class="mt-5 w-fit font-mono text-xs px-3 py-2 bg-yellow-100 border-2 border-yellow-300 rounded-lg shadow-md">
+Nexa Games Hashes
+Generated: Saturday, May 13, 2023 4:26 PM UTC
+Hash count: 10,000
+Epoch: 1
+
+0001_private.txt
+a359601231f93af0b35be9ddd2abe8daedd890577ffb7d36e34aba81dc9777134d788ff360c2b2cab1b23a711848610e2bd78ccce0b4424f477b2b9c50891adf
+
+0001_public.txt
+6cf09a52bf6638df7f3686af6150341bc551648b50c7393bf94b2d105c78f97eca71b048d8f6d18e0e3c26f42b5a46f6e82d6db41c71f9f328c28783de600886
+        </pre>
+
+        <section class="w-fit mt-5 px-3 py-2 bg-purple-700 border-2 border-purple-900 rounded-md shadow-md">
+            <h2 class="text-purple-200 text-lg font-bold">
+                SHA-1 Proof
+            </h2>
+
+            <h2 class="text-purple-200 font-medium">
+                a39764e3f0bcb442f13888564e18fc5262f3f1f5
+            </h2>
+
+            <section class="px-3 py-2">
+                <h3 class="text-sm">
+                    Online SHA-1 Calculator
+                </h3>
+
+                <NuxtLink to="https://emn178.github.io/online-tools/sha1.html" target="_blank" class="text-sm text-blue-100 font-medium hover:underline">
+                    https://emn178.github.io/online-tools/sha1.html
+                </NuxtLink>
+            </section>
+        </section>
+
+        <section class="w-fit mt-5 px-3 py-2 bg-purple-700 border-2 border-purple-900 rounded-md shadow-md">
+            <h2 class="text-purple-200 text-lg font-bold">
+                On-chain (OP_RETURN) Proof
+            </h2>
+
+            <NuxtLink to="https://explorer.nexa.org/" target="_blank" class="text-sm text-blue-100 font-medium hover:underline">
+                [ put transaction id here ]
+            </NuxtLink>
+        </section>
+    </main>
+
+    <Footer />
+</template>
--- web/pages/fairplay/index.vue
+++ web/pages/fairplay/index.vue
@@ -19,29 +19,53 @@ useHead({
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur assumenda velit animi aut ipsa ab vero perferendis nam consequuntur sequi qui deleniti neque facere, laudantium dolore officiis earum tempore nesciunt.
         </p>
 
-        <h2>playid: {{$route.params.playid}}</h2>
-
         <div class="w-full py-5 flex justify-center">
             <input
                 type="text"
-                placeholder="Type or copy/paste your Player ID here"
+                placeholder="Type or copy/paste your Game ID or Play ID here"
                 class="w-1/2 py-2 px-5 bg-yellow-100 border-4 border-yellow-300 text-lg text-yellow-900 placeholder-yellow-900 rounded-lg"
             />
         </div>
 
         <section>
-            <h2>Hashes Lists <em>(by Epoch)</em></h2>
+            <h2 class="text-xl font-medium">
+                Hashes Lists <em>(by Epoch)</em>
+            </h2>
 
             <blockquote class="text-sm text-gray-500 w-1/2">
-                Hashes may be independently verified by <span class="text-indigo-500 font-medium">ANY SHA-1 and SHA-512</span> tools available to you.
-                We recommend <NuxtLink to="https://md5file.com/calculator" target="_blank" class="text-blue-500 font-medium hover:underline">https://md5file.com/calculator</NuxtLink>
+                Hashes and attestations may be independently verified by ANY available <span class="text-indigo-500 font-medium">SHA-512</span> or <span class="text-indigo-500 font-medium">SHA-1</span> calculators.
+                We recommend <NuxtLink to="https://md5file.com/calculator" target="_blank" class="text-blue-500 font-medium hover:underline">https://md5file.com/calculator</NuxtLink> and <NuxtLink to="https://emn178.github.io/online-tools/sha1.html" target="_blank" class="text-blue-500 font-medium hover:underline">https://emn178.github.io/online-tools/sha1.html</NuxtLink> respectively.
             </blockquote>
 
-            <ol class="list-decimal pt-3 pl-10">
-                <li>
-                    <a href="/hashes/0001_public.txt" class="block text-blue-500 font-medium hover:underline">0001_public.txt</a>
-                    <a href="/hashes/0001_public_checksum.txt" class="block text-blue-500 font-medium hover:underline">0001_public_checksum.txt</a>
-                    <h3>SHA-512 -> 6cf09a52bf6638df ... 28c28783de600886</h3>
+            <ol class="mt-5">
+                <li class="px-3 py-2 bg-purple-100 border-2 border-purple-300 rounded-lg shadow-md">
+                    <h2 class="text-base text-purple-500 font-medium uppercase">
+                        Epoch #1
+                    </h2>
+
+                    <div class="pl-5">
+                        <a href="/hashes/0001_public.txt" class="block text-blue-500 font-medium hover:underline">
+                            0001_public.txt
+                        </a>
+
+                        <a href="/hashes/0001_public_checksum.txt" class="block text-blue-500 font-medium hover:underline">
+                            0001_public_checksum.txt
+                        </a>
+
+                        <NuxtLink to="/epoch/1" class="block text-blue-500 font-medium hover:underline">
+                            Attestation
+                        </NuxtLink>
+                    </div>
+
+                    <section class="mt-2 w-fit px-3 py-1 border-2 border-purple-900 bg-purple-700 rounded-lg shadow">
+                        <h3 class="text-purple-100 text-lg font-medium">
+                            SHA-512 Proof
+                        </h3>
+
+                        <h3 class="text-purple-100 font-medium text-xs truncate">
+                            6cf09a52bf6638df7f3686af6150341bc551648b50c7393bf94b2d105c78f97eca71b048d8f6d18e0e3c26f42b5a46f6e82d6db41c71f9f328c28783de600886
+                        </h3>
+                    </section>
                 </li>
             </ol>
         </section>
