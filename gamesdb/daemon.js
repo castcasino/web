fa3b385ca893b7e13a0de1a0d1e6cd1569c921d6
Shomari <nyusternie@sdot.io>
2023-05-15 22:41:54 -0400

    Add db to daemon.

--- gamesdb/daemon.js
+++ gamesdb/daemon.js
@@ -1,200 +1,52 @@
 /* Import modules. */
-import { callNode } from '@nexajs/rpc'
 import moment from 'moment'
 import PouchDB from 'pouchdb'
 import { v4 as uuidv4 } from 'uuid'
+import { Wallet } from '@nexajs/wallet'
 
 /* Import handlers. */
-import handleAddress from './handlers/address.js'
-import handleOutpoint from './handlers/outpoint.js'
+// import handleAddress from './handlers/address.js'
+// import handleOutpoint from './handlers/outpoint.js'
 
 /* Initialize databases. */
-const blocksDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/blocks`)
 const logsDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/logs`)
-const statusDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/status`)
-const transactionsDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/transactions`)
-
-/* Set node options. */
-const RPC_OPTIONS = {
-    username: 'user', // required
-    password: 'password', // required
-    host: '127.0.0.1', // (optional) default is localhost (127.0.0.1)
-    port: '7227', // (optional) default is 7227
-}
-
-/* Set constants. */
-const LOCAL_HOST = '127.0.0.1'
+const walletDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/wallet`)
 
 /* Initialize locals. */
-let blockchainInfo
 let response
 
-const decodeRawTransaction = async (_rawTx) => {
-    /* Set method. */
-    const method = 'decoderawtransaction'
-
-    /* Set parameters. */
-    const params = [_rawTx]
-
-    /* Execute JSON-RPC request. */
-    const response = await callNode(method, params, RPC_OPTIONS)
-    // console.log('\nJSON-RPC response:\n%s', response)
-
-    /* Return response. */
-    return response
-}
-
-const getBlock = async (_blockHash) => {
-    /* Set method. */
-    const method = 'getblock'
-
-    /* Set parameters. */
-    const params = [_blockHash]
-
-    /* Set node options. */
-    const options = {
-        username: 'user', // required
-        password: 'password', // required
-        host: '127.0.0.1', // (optional) default is localhost (127.0.0.1)
-        port: '7227', // (optional) default is 7227
-    }
-
-    /* Execute JSON-RPC request. */
-    const response = await callNode(method, params, RPC_OPTIONS)
-    // console.log('\nJSON-RPC response:\n%s', response)
-
-    /* Return response. */
-    return response
-}
-
-const getTransaction = async (_txidem) => {
-    /* Set method. */
-    const method = 'getrawtransaction'
-
-    /* Set parameters. */
-    const params = [_txidem, true]
-
-    /* Execute JSON-RPC request. */
-    const response = await callNode(method, params, RPC_OPTIONS)
-    // console.log('\nJSON-RPC response:\n%s', response)
-
-    /* Return response. */
-    return response
-}
-
-const getBlockchainInfo = async () => {
-    /* Set method. */
-    const method = 'getblockchaininfo'
-
-    /* Set parameters. */
-    const params = []
-
-    /* Set node options. */
-    const options = {
-        username: 'user', // required
-        password: 'password', // required
-        host: '127.0.0.1', // (optional) default is localhost (127.0.0.1)
-        port: '7227', // (optional) default is 7227
-    }
-
-    /* Execute JSON-RPC request. */
-    const response = await callNode(method, params, RPC_OPTIONS)
-
-    /* Return response. */
-    return response
-}
-
-console.info('\n  Starting Nexa Database daemon...\n')
+console.info('\n  Starting Nexa Games daemon...\n')
 
 
 /**
- * Check Database Syncronization
+ * Wallet Queue
  *
- * Performs a check to make sure we have indexed up to the
- * latest block height.
+ * Performs a check to make sure we have transacted all pending
+ * transactions queued in the wallet.
  */
-const checkDbSync = async () => {
-    console.info('\n  Checking database sync...\n')
+const handleWalletQueue = async () => {
+    console.info('\n  Checking wallet queue...\n')
 
-    let system
+    let queue
 
-    system = await statusDb
-        .get('system')
-        .catch(err => console.error(err))
-    console.log('SYSTEM', system)
+    queue = await walletDb.query('api/isPending', {
+        include_docs: true,
+    })
+    .catch(err => console.error(err))
+    console.log('QUEUE', queue)
 
-    if (blockchainInfo?.blocks > system?.idxHeight) {
-        console.log('\n\n  Starting database sycn...\n')
+}
 
-        /* Handle new blocks. */
-        for (let i = system.idxHeight + 1; i <= blockchainInfo.blocks; i++) {
-            /* Request block at height. */
-            const block = await getBlock(i)
-                .catch(err => {
-                    console.error(err)
-                })
-            // console.log(`BLOCK #${i}`, block)
+const handleQueue = async () => {
 
-            /* Save block to storage. */
-            blocksDb.put({
-                _id: block.height.toString(),
-                ...block,
-            })
-            .catch(err => {
-                console.error(err)
-            })
-
-            // NOTE: Block MUST contain at least the Coinbase transaction.
-            if (block?.txidem) {
-                for (let j = 0; j < block.txidem.length; j++) {
-                    /* Set transaction idem. */
-                    const txidem = block.txidem[j]
-
-                    /* Request transaction details. */
-                    const tx = await getTransaction(txidem)
-                        .catch(err => {
-                            console.error(err)
-                        })
-                    // console.log(`TRANSACTION [${txidem}]`, tx)
-
-                    /* Save transaction to storage. */
-                    transactionsDb.put({
-                        _id: tx.txidem,
-                        ...tx
-                    })
-                    .catch(err => {
-                        console.error(err)
-                    })
-                }
-            }
-
-            /* Retrieve (latest) System status. */
-            const updatedSystem = await statusDb
-                .get('system')
-                .catch(err => console.error(err))
-            // console.log('UPDATED SYSTEM', system)
-
-            /* Set new indexed height. */
-            updatedSystem.idxHeight = i
-            updatedSystem.updatedAt = moment().unix()
-
-            /* Save (updated) System status to storage. */
-            await statusDb
-                .put(updatedSystem)
-                .catch(err => console.error(err))
-        }
-    }
 }
 
 ;(async () => {
-    /* Request Blockchain information. */
-    blockchainInfo = await getBlockchainInfo()
-    console.log('\n\n  Blockchain info:\n', blockchainInfo)
-
     setInterval(() => {
-        console.log('Checking queue...')
+        console.log('Managing queue...')
+        handleWalletQueue()
     }, 5000)
 
 })()
 
-// checkDbSync()
+// handleWalletQueue()
