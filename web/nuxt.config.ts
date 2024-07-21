fb99e93493a771dcf8e4368b64fc7300bc254bc2
Shomari <nyusternie@sdot.io>
2024-07-21 13:37:47 -0400

    Update Docker configs + all Nexa libs to latest.

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
         head: {
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1',
-            title: 'Nexa Games',
+            title: 'Nexa Games — 100% Provably Fair Gaming',
             meta: [
                 { name: 'description', content: 'The first fairplay, permissionless real-money gaming network.' },
             ],
