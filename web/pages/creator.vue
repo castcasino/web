5e0b6af813af2a8fa416f54248fbcb4a8465ca72
Shomari <nyusternie@sdot.io>
2024-11-20 15:15:41 -0500

    Add support for a composer actions.

---
+++ web/pages/creator.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+useHead({
+    title: 'Game Creator — Cast Casino',
+    meta: [
+        { name: 'description', content: 'Nexa Gaming offers a revolutionary blockchain experience.' }
+    ],
+})
+
+/* Initialize stores. */
+import { useSystemStore } from '@/stores/system'
+
+/* Initialize System. */
+const System = useSystemStore()
+
+// onMounted(() => {
+//     console.log('Mounted!')
+//     // Now it's safe to perform setup operations.
+// })
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+
+</script>
+
+<template>
+    <main class="max-w-5xl mx-auto my-10">
+        <h1 class="text-5xl font-medium">
+            Game Creator
+        </h1>
+
+        <p>
+            Let's create brand NEW game and share it with your community.
+        </p>
+    </main>
+</template>
---
+++ web/server/routes/_action.ts
@@ -0,0 +1,14 @@
+export default defineEventHandler((event) => {
+    /* Initialize locals. */
+    let form
+
+    /* Build form. */
+    form = {
+        type: 'form',
+        title: 'Game Creator',
+        url: 'https://cast.casino/creator',
+    }
+
+    /* Return statistics. */
+    return stats
+})
