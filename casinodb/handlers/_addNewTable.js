0c280106c35a94a6553609ec7020bbb7f699f8a8
Shomari <nyusternie@sdot.io>
2024-12-14 14:47:21 -0500

    Parse table info.

--- casinodb/handlers/_addNewTable.js
+++ casinodb/handlers/_addNewTable.js
@@ -13,7 +13,10 @@ export default async (_tableid) => {
 console.log('ADDING NEW TABLE', _tableid)
 
     /* Initialize locals. */
+    let seated
     let tableInfo
+
+    seated = []
 
     tableInfo = await baseClient.readContract({
         address: CAST_POKER_ADDRESS,
@@ -21,7 +24,24 @@ console.log('ADDING NEW TABLE', _tableid)
         functionName: 'tables',
         args: [_tableid]
     })
-console.log('TABLE INFO', tableInfo)
+console.log('TABLE INFO (raw)', tableInfo)
+
+    tableInfo = {
+        state: tableInfo[0],
+        token: tableInfo[1],
+        host: tableInfo[2],
+        seed: tableInfo[3].toString(),
+        buyin: tableInfo[4].toString(),
+        tts: tableInfo[5].toString(),
+        pot: tableInfo[6].toString(),
+        paid: tableInfo[7].toString(),
+        seats: tableInfo[8],
+        fomo: tableInfo[9],
+        theme: tableInfo[10],
+        community: tableInfo[11],
+        seated,
+    }
+console.log('TABLE INFO (parsed)', tableInfo)
 
     return true
 }
