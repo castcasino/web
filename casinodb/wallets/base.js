8287a1e278b8ad7e17b224d8a5c275f330a18f93
Shomari <nyusternie@sdot.io>
2024-12-14 21:15:09 -0500

    Debug.

--- casinodb/wallets/base.js
+++ casinodb/wallets/base.js
@@ -4,12 +4,14 @@ import { base } from 'viem/chains'
 
 export default (_mnemonic) => {
     const account = mnemonicToAccount(_mnemonic)
+console.log('ACCOUNT', account)
 
     const client = createWalletClient({
         account,
         chain: base,
         transport: http(),
     })
+console.log('CLIENT', client)
 
     return client
 }
