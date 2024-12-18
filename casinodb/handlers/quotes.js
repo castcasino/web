b0e870496ba590a16d8dee9389618154181f79e1
Shomari <nyusternie@sdot.io>
2024-12-18 18:47:33 -0500

    Enable quotes test.

--- casinodb/handlers/quotes.js
+++ casinodb/handlers/quotes.js
@@ -1,9 +1,42 @@
 /* Import modules. */
+import axios from 'axios'
 import moment from 'moment'
 import PouchDB from 'pouchdb'
 
 /* Initialize databases. */
 const systemDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/system`)
+
+/* Initialize constants. */
+const ENDPOINT = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+const getQuotes = async () => {
+    /* Initialize locals. */
+    let headers
+    let json
+    let response
+
+    /* Set headers. */
+    headers = {
+        'X-CMC_PRO_API_KEY': process.env.CMC_PRO_API_KEY,
+    }
+
+    /* Request data. */
+    response = await axios
+        .get(ENDPOINT, { headers })
+        .catch(err => console.error(err))
+console.log('RESPONSE (quotes)', response)
+
+    /* Validate response. */
+    if (response) {
+        json = response.data
+console.log('RESPONSE (json)', json)
+    } else {
+        json = {}
+    }
+
+    /* Return json. */
+    return json
+}
 
 export default async () => {
 // console.log('MANAGING QUOTES')
@@ -26,6 +59,9 @@ export default async () => {
 
     quotes.DEGEN = await response.json()
 // console.log('QUOTES', quotes)
+
+const test = await getQuotes()
+console.log('QUOTES TEST', test)
 
     response = await systemDb
         .get('quotes')
