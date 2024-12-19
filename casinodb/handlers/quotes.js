b86efda1a242727e8aed961149e57d170179c23d
Shomari <nyusternie@sdot.io>
2024-12-18 19:13:37 -0500

    Fix syntax.

--- casinodb/handlers/quotes.js
+++ casinodb/handlers/quotes.js
@@ -31,9 +31,10 @@ const getQuotes = async () => {
         // quotes = response.data
 // console.log('RESPONSE (quotes)', quotes)
 
+        /* Find quotes. */
         quotes = {
-            ETH: response.data.find(_asset => (_asset.id === 1027) ? _asset.quote : {},
-            DEGEN: response.data.find(_asset => (_asset.id === 30096) ? _asset.quote : {},
+            ETH: response.data.find(_asset => (_asset.id === 1027) ? _asset.quote : {}),
+            DEGEN: response.data.find(_asset => (_asset.id === 30096) ? _asset.quote : {}),
         }
     } else {
         quotes = {}
