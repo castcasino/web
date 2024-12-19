f5677845ff36d29ea1447a60fd25da0b721dc2b7
Shomari <nyusternie@sdot.io>
2024-12-18 19:30:19 -0500

    Debug.

--- casinodb/handlers/quotes.js
+++ casinodb/handlers/quotes.js
@@ -28,14 +28,15 @@ const getQuotes = async () => {
 
     /* Validate response. */
     if (response) {
-        quotes = response.data
-console.log('RESPONSE (quotes)', quotes)
+        // quotes = response.data
+// console.log('RESPONSE (quotes)', quotes)
 
         /* Find quotes. */
         quotes = {
             ETH: response.data.data.find(_asset => (_asset.id === 1027) ? _asset.quote.quote : {}),
             DEGEN: response.data.data.find(_asset => (_asset.id === 30096) ? _asset.quote.quote : {}),
         }
+console.log('RESPONSE (quotes)', quotes)
     } else {
         quotes = {}
     }
