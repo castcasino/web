9fbafcc5c45c8f60a45787667fd4d0a40072490d
Shomari <nyusternie@sdot.io>
2024-12-18 22:43:26 -0500

    Fix db name.

--- api/handlers/admin/getLogs.js
+++ api/handlers/admin/getLogs.js
@@ -4,7 +4,7 @@ import PouchDB from 'pouchdb'
 import { v4 as uuidv4 } from 'uuid'
 
 /* Initialize databases. */
-const logsDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/logs`)
+const sessionsDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/sessions`)
 
 export default async (_req, _res) => {
     /* Initialize locals. */
@@ -20,7 +20,7 @@ export default async (_req, _res) => {
     sessionid = body.sessionid
 // console.log('SESSION ID', sessionid)
 
-    response = await logsDb
+    response = await sessionsDb
         .query('api/isCastEmbed', {
             limit: 50,
             include_docs: true,
