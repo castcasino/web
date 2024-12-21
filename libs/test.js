ba857ca5f4c7af531fc85de4166f67d1ddcc26f6
Shomari <nyusternie@sdot.io>
2024-12-21 05:18:19 -0500

    Update docker file.

--- libs/test.js
+++ libs/test.js
@@ -1,17 +1,18 @@
 import { _Hand as Hand } from './pokersolver.js'
-console.log('HAND', Hand)
+// console.log('HAND', Hand)
 
-var hand1 = Hand.solve(['Ad', 'As', 'Jc', 'Th', '2d', '3c', 'Kd']);
-console.log('HAND 1', hand1)
+var hand1 = Hand.solve(['AD', 'AS', 'JC', 'TH', '2D', '3C', 'KD']);
+console.log('HAND 1', hand1.descr)
 
-var hand2 = Hand.solve(['Ad', 'As', 'Jc', 'Th', '2d', 'Qs', 'Qd']);
-console.log('HAND 2', hand2)
-
-var hand3 = Hand.solve(['Ad', 'As', 'Jc', 'Th', '2d', 'Qs', 'Qd']);
-console.log('HAND 3', hand3)
+var hand2 = Hand.solve(['AD', 'AS', 'JC', 'TH', '2D', 'QS', 'JD']);
+console.log('HAND 2', hand2.descr)
 
 var winner = Hand.winners([hand1, hand2]); // hand2
-console.log('WINNER', winner.length, winner)
+console.log('WINNER', winner.length, winner.map(_winner => _winner.descr))
 
-var winners = Hand.winners([hand2, hand3]); // hand2
-console.log('WINNER', winners.length, winners)
+var hand3 = Hand.solve(['AD', 'AS', 'JC', 'TH', '2D', 'QS', 'QD']);
+console.log('HAND 3', hand3.descr)
+
+var winners = Hand.winners([hand2, hand3]); // tie
+console.log('WINNER', winners.length, winners.map(_winner => _winner.name))
+console.log('WINNER', winners.length, winners.map(_winner => _winner.descr))
