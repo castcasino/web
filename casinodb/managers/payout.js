f4165320a889ac3328b32e834ce60a7ac709502e
Shomari <nyusternie@sdot.io>
2024-12-23 04:13:19 -0500

    Test payout params.

--- casinodb/managers/payout.js
+++ casinodb/managers/payout.js
@@ -33,6 +33,8 @@ const MIN_SHOWDOWN_BLOCKS = 50
 export default async () => {
 console.log('MANAGING PAYOUT')
     /* Initialize locals. */
+    let params
+    let response
     let tableInfo
 
 //     const blk = await systemDb
@@ -80,157 +82,20 @@ console.log('TABLE INFO', tableInfo)
     }
 console.log('TABLE INFO (packaged)', tableInfo)
 
-return
-
-    response = await pokerTablesDb
-        .query('api/hasCommunity', {
-            include_docs: true,
-        }).catch(err => console.error(err))
-// console.log('RESPONSE (unseated tables)', response)
-
-    /* Validate response. */
-    if (!response || response.total_rows === 0) {
-        return console.log('  All tables are seated!')
-    }
-
-    /* Set (open) tables. */
-    // tables = response.rows.map((_unset) => {
-    //     return _unset.doc
-    // })
-
-    /* Initialize tables. */
-    tables = {}
-
-    /* Remodel tables data (to object). */
-    response.rows.forEach(_table => {
-        /* Set id. */
-        const id = _table.id
-
-        /* Set table. */
-        tables[id] = _table.doc
-
-        /* Sanitize. */
-        delete tables[id]._id
-        delete tables[id]._rev
-    })
-console.log('OPEN TABLES', tables)
-
-    blockIdx = tables[tableid]?.community?.flop1?.blockIdx
-console.log('BLOCK INDEX', blockIdx)
-
-    response = await blocksBaseDb
-        .get(blockIdx.toString(), { include_docs: true })
-        .catch(err => console.error(err))
-console.log('RESPONSE (time blocks)', response)
-
-    if (typeof response !== 'undefined') {
-        timestamp = response.timestamp
-    }
-
-    if (typeof timestamp === 'undefined') {
-        throw new Error('ERROR: Invalid timestamp in blocks database.')
-    }
-console.log('TIMESTAMP', timestamp)
-
-    /* Set time to sit. */
-    tts = tables[tableid]?.tts
-console.log('TIME TO SIT', tts)
-
-    /* Set showdown timestamp. */
-    showdownAt = timestamp + Number(tts)
-console.log('SHOWDOWN AT', showdownAt)
-
-    response = await blocksBaseDb
-        .query('api/byTimestamp', {
-            startkey: showdownAt,
-            limit: MIN_SHOWDOWN_BLOCKS,
-            include_docs: true,
-        }).catch(err => console.error(err))
-// console.log('RESPONSE (time blocks)', response)
-
-    /* Set (time) blocks. */
-    blocks = response.rows.map((_unset) => {
-        return _unset.doc
-    })
-console.log('SHOWDOWN BLOCKS', blocks.length, blocks[0])
-
-
-    activeDeck = fullDeck()
-    // console.log('(FULL) DECK', activeDeck.length, activeDeck)
-
-console.log('ACTIVE DECK', activeDeck.length, activeDeck)
-    dealCards(
-        activeDeck, tables[tableid].community.flop1.blockHash.slice(2), 1)
-// console.log('ACTIVE DECK', activeDeck.length)
-    dealCards(
-        activeDeck, tables[tableid].community.flop2.blockHash.slice(2), 1)
-// console.log('ACTIVE DECK', activeDeck.length)
-    dealCards(
-        activeDeck, tables[tableid].community.flop3.blockHash.slice(2), 1)
-// console.log('ACTIVE DECK', activeDeck.length)
-    dealCards(
-        activeDeck, tables[tableid].community.turn.blockHash.slice(2), 1)
-// console.log('ACTIVE DECK', activeDeck.length)
-    dealCards(
-        activeDeck, tables[tableid].community.river.blockHash.slice(2), 1)
-// console.log('ACTIVE DECK', activeDeck.length)
-
-    /* Initialize selected handler. */
-    selected = []
-
-    selected.push(dealCards(
-        activeDeck, blocks[0].hash.slice(2), 2))
-// console.log('ACTIVE DECK', activeDeck.length)
-    selected.push(dealCards(
-        activeDeck, blocks[1].hash.slice(2), 2))
-// console.log('ACTIVE DECK', activeDeck.length)
-
-console.log('SELECTED', selected)
-console.log('ACTIVE DECK', activeDeck.length, activeDeck)
-
-    /* Initialize seated handler. */
-    seated = []
-
-    seat = {
-        address: tables[tableid].seated[0],
-        hole1: selected[0][0],
-        hole1Idx: idxLookup(selected[0][0]),
-        hole2: selected[0][1],
-        hole2Idx: idxLookup(selected[0][1]),
-        blockIdx: blocks[0]._id,
-        blockHash: blocks[0].hash,
-    }
-    seated.push(seat)
-console.log('SEAT-1', seat)
-
-    seat = {
-        address: tables[tableid].seated[1],
-        hole1: selected[1][0],
-        hole1Idx: idxLookup(selected[1][0]),
-        hole2: selected[1][1],
-        hole2Idx: idxLookup(selected[1][1]),
-        blockIdx: blocks[1]._id,
-        blockHash: blocks[1].hash,
-    }
-    seated.push(seat)
-console.log('SEAT-2', seat)
-
-    /* Set seat index. */
-    seatIdx = 1
+// return
 
     params = {
         address: CAST_POKER_ADDRESS,
         abi: castPokerAbi,
-        functionName: 'dealCards',
+        functionName: 'payout',
         args: [
             BigInt(tableid),
-            seated[seatIdx].address,
-            seated[seatIdx].hole1Idx,
-            seated[seatIdx].hole2Idx,
+            '0x27a9b30DBe015842098F4CD31f0301a1cEE74bfe',
+            9500000000000n,
         ],
         account: baseAccount().account,
     }
-// console.log('CONTRACT PARAMS (deal cards)', params)
+console.log('CONTRACT PARAMS (payout)', params)
 
     /* Validate hostess. */
 //     response = await baseClient
@@ -241,42 +106,4 @@ console.log('SEAT-2', seat)
 //         })
 // console.log('RESPONSE (simulate deal cards)', response)
 
-return
-
-//     if (typeof response !== 'undefined' && response.request) {
-//         response = await baseAccount()
-//             .writeContract(response.request)
-//             .catch(err => console.error(err))
-// console.log('RESPONSE (write deal cards)', response)
-//     }
-
-    params = {
-        address: CAST_POKER_ADDRESS,
-        abi: castPokerAbi,
-        functionName: 'showdown',
-        args: [ BigInt(tableid) ],
-        account: baseAccount().account,
-    }
-// console.log('CONTRACT PARAMS (showdown)', params)
-
-    /* Validate hostess. */
-    response = await baseClient
-        .simulateContract(params)
-        .catch(err => {
-            // console.error(err)
-            console.error(err.message)
-        })
-console.log('RESPONSE (simulate showdown)', response)
-
-    if (typeof response !== 'undefined' && response.request) {
-        response = await baseAccount()
-            .writeContract(response.request)
-            .catch(err => console.error(err))
-console.log('RESPONSE (write showdown)', response)
-    }
-
-
-    // const hash = new Sha256();
-    // hash.update('some data');
-    // const result = await hash.digest()
 }
