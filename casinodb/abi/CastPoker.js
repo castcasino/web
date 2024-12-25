15c5107724e442f5cfe1743592576fbc126c97e3
Shomari <nyusternie@sdot.io>
2024-12-25 14:24:17 -0500

    Update contract abi.

--- casinodb/abi/CastPoker.js
+++ casinodb/abi/CastPoker.js
@@ -1,5 +1,23 @@
 export default [
 	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tableid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_seed",
+				"type": "uint256"
+			}
+		],
+		"name": "buyIn",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
 		"type": "constructor"
@@ -53,6 +71,25 @@ export default [
 		"type": "event"
 	},
 	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tableid",
+				"type": "uint256"
+			}
+		],
+		"name": "closeTable",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
 		"anonymous": false,
 		"inputs": [
 			{
@@ -96,6 +133,72 @@ export default [
 		"type": "event"
 	},
 	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tableid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address[]",
+				"name": "_playersPool",
+				"type": "address[]"
+			},
+			{
+				"internalType": "int8[]",
+				"name": "_hole1Pool",
+				"type": "int8[]"
+			},
+			{
+				"internalType": "int8[]",
+				"name": "_hole2Pool",
+				"type": "int8[]"
+			}
+		],
+		"name": "dealCards",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tableid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint8",
+				"name": "_flop1",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint8",
+				"name": "_flop2",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint8",
+				"name": "_flop3",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint8",
+				"name": "_turn",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint8",
+				"name": "_river",
+				"type": "uint8"
+			}
+		],
+		"name": "dealCommunityCards",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
 		"anonymous": false,
 		"inputs": [
 			{
@@ -113,6 +216,35 @@ export default [
 		],
 		"name": "OwnershipTransferred",
 		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tableid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_player",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "payout",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"anonymous": false,
@@ -169,6 +301,131 @@ export default [
 		],
 		"name": "PlayerCardsDealt",
 		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "tableid",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address[]",
+				"name": "players",
+				"type": "address[]"
+			},
+			{
+				"indexed": false,
+				"internalType": "int8[]",
+				"name": "hole1",
+				"type": "int8[]"
+			},
+			{
+				"indexed": false,
+				"internalType": "int8[]",
+				"name": "hole2",
+				"type": "int8[]"
+			}
+		],
+		"name": "PlayersCardsDealt",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "_newSuccessor",
+				"type": "address"
+			}
+		],
+		"name": "setSuccessor",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_seed",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_buyin",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_tts",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint8",
+				"name": "_seats",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint8",
+				"name": "_fomo",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint8",
+				"name": "_theme",
+				"type": "uint8"
+			}
+		],
+		"name": "setTable",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tableid",
+				"type": "uint256"
+			}
+		],
+		"name": "showdown",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"anonymous": false,
@@ -284,28 +541,50 @@ export default [
 		"type": "event"
 	},
 	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
 		"stateMutability": "payable",
 		"type": "fallback"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
 	},
 	{
 		"inputs": [
 			{
 				"internalType": "uint256",
-				"name": "",
+				"name": "_chainid",
 				"type": "uint256"
-			}
-		],
-		"name": "benches",
-		"outputs": [
-			{
-				"internalType": "enum CastPoker.GameplayState",
-				"name": "state",
-				"type": "uint8"
 			},
 			{
 				"internalType": "address",
-				"name": "token",
+				"name": "_assetid",
 				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_player",
+				"type": "address"
+			}
+		],
+		"name": "getChips",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
 			}
 		],
 		"stateMutability": "view",
@@ -319,125 +598,46 @@ export default [
 				"type": "uint256"
 			},
 			{
-				"internalType": "uint256",
-				"name": "_seed",
-				"type": "uint256"
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
 			}
 		],
-		"name": "buyIn",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tableid",
-				"type": "uint256"
-			}
-		],
-		"name": "close",
+		"name": "getPlayer",
 		"outputs": [
 			{
-				"internalType": "bool",
+				"components": [
+					{
+						"internalType": "address",
+						"name": "id",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "seed",
+						"type": "uint256"
+					},
+					{
+						"components": [
+							{
+								"internalType": "int8",
+								"name": "hole1",
+								"type": "int8"
+							},
+							{
+								"internalType": "int8",
+								"name": "hole2",
+								"type": "int8"
+							}
+						],
+						"internalType": "struct CastPoker.PlayerCards",
+						"name": "cards",
+						"type": "tuple"
+					}
+				],
+				"internalType": "struct CastPoker.Player",
 				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tableid",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address",
-				"name": "_player",
-				"type": "address"
-			},
-			{
-				"internalType": "int8",
-				"name": "_hole1",
-				"type": "int8"
-			},
-			{
-				"internalType": "int8",
-				"name": "_hole2",
-				"type": "int8"
-			}
-		],
-		"name": "dealCards",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tableid",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint8",
-				"name": "_flop1",
-				"type": "uint8"
-			},
-			{
-				"internalType": "uint8",
-				"name": "_flop2",
-				"type": "uint8"
-			},
-			{
-				"internalType": "uint8",
-				"name": "_flop3",
-				"type": "uint8"
-			},
-			{
-				"internalType": "uint8",
-				"name": "_turn",
-				"type": "uint8"
-			},
-			{
-				"internalType": "uint8",
-				"name": "_river",
-				"type": "uint8"
-			}
-		],
-		"name": "dealCommunityCards",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_chainid",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address",
-				"name": "_player",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "_assetid",
-				"type": "address"
-			}
-		],
-		"name": "getChips",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
+				"type": "tuple"
 			}
 		],
 		"stateMutability": "view",
@@ -502,6 +702,119 @@ export default [
 		"type": "function"
 	},
 	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tableid",
+				"type": "uint256"
+			}
+		],
+		"name": "getTable",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "enum CastPoker.GameplayState",
+						"name": "state",
+						"type": "uint8"
+					},
+					{
+						"internalType": "address",
+						"name": "token",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "host",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "seed",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "buyin",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "tts",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "pot",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "paid",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint8",
+						"name": "seats",
+						"type": "uint8"
+					},
+					{
+						"internalType": "uint8",
+						"name": "fomo",
+						"type": "uint8"
+					},
+					{
+						"internalType": "uint8",
+						"name": "theme",
+						"type": "uint8"
+					},
+					{
+						"components": [
+							{
+								"internalType": "uint8",
+								"name": "flop1",
+								"type": "uint8"
+							},
+							{
+								"internalType": "uint8",
+								"name": "flop2",
+								"type": "uint8"
+							},
+							{
+								"internalType": "uint8",
+								"name": "flop3",
+								"type": "uint8"
+							},
+							{
+								"internalType": "uint8",
+								"name": "turn",
+								"type": "uint8"
+							},
+							{
+								"internalType": "uint8",
+								"name": "river",
+								"type": "uint8"
+							}
+						],
+						"internalType": "struct CastPoker.CommunityCards",
+						"name": "community",
+						"type": "tuple"
+					},
+					{
+						"internalType": "address[]",
+						"name": "seated",
+						"type": "address[]"
+					}
+				],
+				"internalType": "struct CastPoker.Table",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
 		"inputs": [],
 		"name": "getTotalTables",
 		"outputs": [
@@ -526,311 +839,5 @@ export default [
 		],
 		"stateMutability": "view",
 		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tableid",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address",
-				"name": "_player",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_amount",
-				"type": "uint256"
-			}
-		],
-		"name": "payout",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "players",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "id",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "seed",
-				"type": "uint256"
-			},
-			{
-				"components": [
-					{
-						"internalType": "int8",
-						"name": "hole1",
-						"type": "int8"
-					},
-					{
-						"internalType": "int8",
-						"name": "hole2",
-						"type": "int8"
-					}
-				],
-				"internalType": "struct CastPoker.PlayerCards",
-				"name": "cards",
-				"type": "tuple"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "renounceOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "_token",
-				"type": "address"
-			}
-		],
-		"name": "setBench",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address payable",
-				"name": "_newSuccessor",
-				"type": "address"
-			}
-		],
-		"name": "setSuccessor",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "success",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "_token",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_seed",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_buyin",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_tts",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint8",
-				"name": "_seats",
-				"type": "uint8"
-			},
-			{
-				"internalType": "uint8",
-				"name": "_fomo",
-				"type": "uint8"
-			},
-			{
-				"internalType": "uint8",
-				"name": "_theme",
-				"type": "uint8"
-			}
-		],
-		"name": "setTable",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tableid",
-				"type": "uint256"
-			}
-		],
-		"name": "showdown",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "tables",
-		"outputs": [
-			{
-				"internalType": "enum CastPoker.GameplayState",
-				"name": "state",
-				"type": "uint8"
-			},
-			{
-				"internalType": "address",
-				"name": "token",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "host",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "seed",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "buyin",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "tts",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "pot",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "paid",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint8",
-				"name": "seats",
-				"type": "uint8"
-			},
-			{
-				"internalType": "uint8",
-				"name": "fomo",
-				"type": "uint8"
-			},
-			{
-				"internalType": "uint8",
-				"name": "theme",
-				"type": "uint8"
-			},
-			{
-				"components": [
-					{
-						"internalType": "uint8",
-						"name": "flop1",
-						"type": "uint8"
-					},
-					{
-						"internalType": "uint8",
-						"name": "flop2",
-						"type": "uint8"
-					},
-					{
-						"internalType": "uint8",
-						"name": "flop3",
-						"type": "uint8"
-					},
-					{
-						"internalType": "uint8",
-						"name": "turn",
-						"type": "uint8"
-					},
-					{
-						"internalType": "uint8",
-						"name": "river",
-						"type": "uint8"
-					}
-				],
-				"internalType": "struct CastPoker.CommunityCards",
-				"name": "community",
-				"type": "tuple"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "newOwner",
-				"type": "address"
-			}
-		],
-		"name": "transferOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"stateMutability": "payable",
-		"type": "receive"
 	}
 ]
